13f79107a7b8911612a86fc2ea573177d96bea72
Geoff Greer <geoff@greer.fm>
2017-04-24 16:28:47 -0700

    scripts: Don't require S3 to make a tarball.
    - Don't check AWS creds at the start of make_release_tarball.sh.
    - Copy local installer binaries to release dir instead of grabbing them from s3.
    - Move terraform-specific env vars to get_terraform_bins.sh. They're only used in this one place.
    - Use straightforward string interpolation instead of eval() to construct terraform binary URLs.

--- installer/scripts/release/get_terraform_bins.sh
+++ installer/scripts/release/get_terraform_bins.sh
@@ -3,13 +3,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/common.env.sh"
 
+TERRAFORM_BIN_TMP_DIR="$TMP_DIR/terraform-bin"
+TERRAFORM_BIN_VERSION=0.8.8
+TERRAFORM_BIN_BASE_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_BIN_VERSION}/terraform_${TERRAFORM_BIN_VERSION}"
+
 mkdir -p "${TMP_DIR}" "${TERRAFORM_BIN_TMP_DIR}"
 
 set -x
 
 for os in "linux" "darwin" "windows"; do
-    curl -L $(eval echo ${TERRAFORM_BIN_URL}) -o ${TERRAFORM_BIN_TMP_DIR}/terraform_${os}.zip
-    mkdir -p ${INSTALLER_RELEASE_DIR}/${os}/
-    unzip -o ${TERRAFORM_BIN_TMP_DIR}/terraform_${os}.zip -d ${INSTALLER_RELEASE_DIR}/${os}/
+    curl -L "${TERRAFORM_BIN_BASE_URL}_${os}_amd64.zip" -o "${TERRAFORM_BIN_TMP_DIR}/terraform_${os}.zip"
+    mkdir -p "${INSTALLER_RELEASE_DIR}/${os}/"
+    unzip -o "${TERRAFORM_BIN_TMP_DIR}/terraform_${os}.zip" -d "${INSTALLER_RELEASE_DIR}/${os}/"
     chmod +x ${INSTALLER_RELEASE_DIR}/${os}/terraform*
 done
